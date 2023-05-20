ef284f2d240a3ae9a0ca7e29b4ab1b839a709ab7
Srirag Hariharan <sriraghariharan108@gmail.com>
2023-05-20 16:08:18 +0530

    Added footer to the webpage

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div style={{padding:'20px',
+        textAlign: 'center',
+        padding: '50px',
+        backgroundColor:' #f5f5f5',}}>
+        <p className="text-center">
+            Made in
+            <img style={{width:'35px'}} src="https://w7.pngwing.com/pngs/836/833/png-transparent-round-orange-white-and-green-flag-of-indian-art-flag-of-india-computer-icons-national-flag-indian-flag-blue-flag-orange.png" alt="" />
+            with ❤️
+            <br />
+            <br />
+            @SriragHariharan 
+        </p>
+    </div>
+  )
+}
+
+export default Footer
--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import NavbarB from '../components/NavbarB'
+import Footer from '../components/Footer'
 
 function RootLayout() {
   return (
@@ -9,6 +10,7 @@ function RootLayout() {
         <main>
             <Outlet/>
         </main>
+        <Footer/>
     </div>
   )
 }
